e4615a87ddc81ec5031176f490853663cbcc41f1
Om Dixit <omdixit2826@gmail.com>
2025-12-15 17:17:51 +0530

    Add Shop Your Size component to home page
    - Created `ShopYourSizeComponent` to display a banner for size selection.
    - Integrated the component into the home page layout.
    - Added corresponding HTML and SCSS files for the component.
    - Implemented a click handler for the banner to log interactions and prepare for future navigation.

---
+++ frontend/snitch-app/src/app/components/shop-your-size/shop-your-size.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-your-size',
+  imports: [],
+  templateUrl: './shop-your-size.html',
+  styleUrl: './shop-your-size.scss',
+})
+export class ShopYourSizeComponent {
+  bannerImageUrl = 'https://cdn.shopify.com/s/files/1/0420/7073/7058/files/refresh_18_nov_PLP02.jpg?v=1763468105';
+
+  onBannerClick(): void {
+    console.log('Shop Your Size banner clicked');
+    // TODO: Navigate to size selection page
+  }
+}
+
--- frontend/snitch-app/src/app/pages/home/home.ts
+++ frontend/snitch-app/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { BannerCarouselComponent } from '../../components/banner-carousel/banner-carousel';
 import { FeaturedCategoriesComponent } from '../../components/featured-categories/featured-categories';
+import { ShopYourSizeComponent } from '../../components/shop-your-size/shop-your-size';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, BannerCarouselComponent, FeaturedCategoriesComponent],
+  imports: [Header, BannerCarouselComponent, FeaturedCategoriesComponent, ShopYourSizeComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
